5a506e4cb844cbb045210c4459b03c4ecc4dbe79
Makhsin <pmnknb@gmail.com>
2021-01-13 10:00:22 +0700

    update

---
+++ hello.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Hello</title>
+    </head>
+    <body>
+        <?php
+        echo "<h1>Hello</h1><br>";
+        echo "<h2>Bangkok</h2>";
+            ?>
+    </body>
+</html>
