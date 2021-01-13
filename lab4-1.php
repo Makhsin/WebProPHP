0494d0db687ca68237d47795fa384ac520cf2c6e
Makhsin <pmnknb@gmail.com>
2021-01-13 11:07:39 +0700

    update

---
+++ lab4-1.php
@@ -0,0 +1,25 @@
+<html>
+    <head>
+        <title>กำหนดค่าตัวแปร</title>
+    </head>
+    <body>
+        <?php
+        $x = 100;
+        $y = "200";
+        $z = '300';
+        $add = $x + $y;
+        $sub = $z - $x;
+        $sar = $y * $z;
+        $bar = $sar / $z;
+        $message ="Hello World";
+        echo $x; echo "<br />";
+        echo $y; echo "<br />";
+        echo $z; echo "<br />";
+        echo $add; echo "<br />";
+        echo $sub; echo "<br />";
+        echo $sar; echo "<br />";
+        echo $bar; echo "<br />";
+        echo $message; echo "<br />";
+            ?>
+    </body>
+</html>
